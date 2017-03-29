23225f0faa4eb79b489b05fd87d8f02c54a3cccd
Xiaobaxi <silencon@gmail.com>
2017-03-29 16:41:22 +0800

    init the trace for dubbo used spring-cloud-sleuth

---
+++ src/main/java/io/github/xiaobaxi/sleuth/dubbo/ConsumerSpanFilter.java
@@ -0,0 +1,49 @@
+package io.github.xiaobaxi.sleuth.dubbo;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.*;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.SpanInjector;
+import org.springframework.cloud.sleuth.Tracer;
+
+/**
+ * 
+ * @author fangzhibin
+ *
+ */
+@Activate(group = { Constants.CONSUMER }, order = -9000)
+public class ConsumerSpanFilter implements Filter {
+
+	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+		boolean isTraceDubbo = false;
+		Tracer tracer = null;
+		SpanInjector<RpcContext> spanInjector = null;
+		try {
+			tracer = ApplicationContextAwareBean.CONTEXT.getBean(Tracer.class);
+			spanInjector = ApplicationContextAwareBean.CONTEXT.getBean(DubboSpanInjector.class);
+			isTraceDubbo = (tracer != null && spanInjector != null);
+			if (isTraceDubbo) {
+				String spanName = invoker.getUrl().getParameter("interface") + ":" + invocation.getMethodName() + ":"
+						+ invoker.getUrl().getParameter("version") + "(" + invoker.getUrl().getHost() + ")";
+				Span newSpan = tracer.createSpan(spanName);
+				spanInjector.inject(newSpan, RpcContext.getContext());
+				newSpan.logEvent(Span.CLIENT_SEND);
+
+			}
+
+			Result result = invoker.invoke(invocation);
+			return result;
+
+		} finally {
+			if (isTraceDubbo) {
+				if (tracer.isTracing()) {
+					tracer.getCurrentSpan().logEvent(Span.CLIENT_RECV);
+					tracer.close(tracer.getCurrentSpan());
+				}
+
+			}
+		}
+	}
+
+}
---
+++ src/test/java/io/github/xiaobaxi/sleuth/dubbo/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author fangzhibin
+ *
+ */
+package io.github.xiaobaxi.sleuth.dubbo.test;
